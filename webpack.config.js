e2a9b2122a21d0cea2f83cff9797c3b9f94da07e
Renat Hi <renathi@MacBook-Pro-Renat.local>
2021-08-17 23:02:36 +0300

    добавление проекта

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var ghpages = require('gh-pages');
+ghpages.publish('dist', function(err) {});
 
 module.exports = {
   
