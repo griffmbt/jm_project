162ec878f662664bc4c17dc0ae87001f091a506a
Renat Hi <renathi@MacBook-Pro-Renat.local>
2021-08-24 11:59:13 +0300

    анимация на появление для modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 import { body, aside } from "./index";
 
 const modals = document.querySelectorAll(".modal");
+const modalBody = document.querySelectorAll(".modal__body");
 
 const chatButtonHeader = document.querySelector(".header__button--chat");
 const chatButtonAside = document.querySelector(".button--chat");
@@ -14,18 +15,32 @@ const modalButtonClose = document.querySelectorAll(".modal__close");
 const modalFeedback = document.querySelector(".modal__feedback");
 const modalCall = document.querySelector(".modal__call");
 
+const addClassModalBody = function () {
+  modalBody.forEach((body) => {
+    body.classList.add("appear__right");
+  });
+};
+
+const removeClassModalBody = function () {
+  modalBody.forEach((body) => {
+    body.classList.remove("appear__right");
+  });
+};
+
 const removeFeedback = function () {
   modalFeedback.classList.remove("not-visible");
   body.classList.add("restrictScroll");
+  addClassModalBody();
 };
 const removeCall = function () {
   modalCall.classList.remove("not-visible");
   body.classList.add("restrictScroll");
+  addClassModalBody();
 };
 
 const addFeedback = function () {
   modalFeedback.classList.add("not-visible");
-
+  removeClassModalBody();
   if (window.innerWidth <= 768) {
     if (!aside.classList.contains("not-visible")) {
       body.classList.add("restrictScroll");
@@ -39,7 +54,7 @@ const addFeedback = function () {
 
 const addCall = function () {
   modalCall.classList.add("not-visible");
-
+  removeClassModalBody();
   if (window.innerWidth <= 768) {
     if (!aside.classList.contains("not-visible")) {
       body.classList.add("restrictScroll");
